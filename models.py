b3509282abcf779ad153d091bdc69ca1a0ce0af1
u1942817 <92723460+u1942817@users.noreply.github.com>
2022-01-28 18:25:44 +0000

    Login and Register files created
    Login and Register files created

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
@@ -58,9 +59,25 @@ def homepage():
 def resource_board():
     return render_template('resource_board.html', title='Resource Board')
 
-@app.route('/login')
-def login():
-    return render_template('login.html', title='Login')
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login_two", methods=['GET', 'POST'])
+def login_two():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login_two.html', title='Login', form=form)
 
 @app.route('/dashboard')
 def dashboard():
